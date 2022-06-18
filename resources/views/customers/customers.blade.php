792ddd5ce660ff0fa9e82dff4b4e27f959f5a856
Orliany Quezada <orliany.quezada@gmail.com>
2022-06-18 16:54:19 -0400

    Se agrego customers a una carpeta

---
+++ resources/views/customers/customers.blade.php
@@ -0,0 +1,226 @@
+@extends('adminlte::page')
+
+@section('title', 'Customers')
+
+@section('content_header')
+    <!-- Container's info -->
+    <div class="container mt-3 col-lg-8">
+        <!-- Messages alert -->
+        @if (session('flash'))
+            <x-adminlte-alert theme="success" title="{{ session('flash') }}" dismissable></x-adminlte-alert>
+        @endif
+        @if ($errors->any())
+            <x-adminlte-alert theme="danger" titlle="Error" dismissable>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </x-adminlte-alert>
+        @endif
+        <div class="card border-white shadow">
+            <div class="card-body">
+                <div class="row justify-content-between">
+                    <div class="col-12 col-lg-3">
+                        <h3><strong>Customers</strong></h3>
+                    </div>
+                    <div class="col-12 col-lg-3">
+                        <!-- customer registration button -->
+                        <button type="button" class="btn btn-warning shadow-sm w-100" data-toggle="modal" data-target="#insertCustomer">
+                            <strong>Registration</strong>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
+
+@section('content')
+    <div class="container col-lg-8">
+        <!-- Customer's table -->
+        <div class="card shadow border-white">
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-hover" id="dtCustomers" class="display">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Customer</th>
+                                <th>Phone</th>
+                                <th>Edit</th>
+                                <th>Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+        <!-- Modal's insert -->
+        <div class="modal fade" id="insertCustomer" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="insertCustomerLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="insertCustomerLabel"><strong>Customer Registration</strong></h5>
+                    </div>
+                    <div class="modal-body">
+                        <form action="{{ route('customerRegisterPost') }}" method="post" autocomplete="off" id="customerRegister">
+                            @csrf
+                            <div class="row">
+                                <div class="col-12 col-lg-7">
+                                    <div class="form-group">
+                                        <label for="name">Name</label>
+                                        <input type="text" name="name_cu" id="name" class="form-control shadow-sm" placeholder="Name">
+                                    </div>
+                                </div>
+                                <div class="col-12 col-lg-5">
+                                    <div class="form-group">
+                                        <label for="phone">Phone number</label>
+                                        <input type="tel" name="phone_cu" id="phone" class="form-control shadow-sm" placeholder="Phone number">
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" form="customerRegister" class="btn btn-warning">Save</button>
+                        <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <!-- Modal's update -->
+        <div class="modal fade" id="editCustomer" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="editCustomerLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="editCustomerLabel"><strong>Edit Customer</strong></h5>
+                    </div>
+                    <div class="modal-body">
+                        <form id="updateCustomer">
+                            <input type="hidden" name="id" id="idCustomerEdit">
+                            <input type="hidden" name="_token" id="token" value="{{ csrf_token() }}">
+                            <div class="row">
+                                <div class="col-12 col-lg-7">
+                                    <div class="form-group">
+                                        <label for="name_cu">Name</label>
+                                        <input type="text" id="nameEdit" name="name_cu" class="form-control shadow-sm">
+                                    </div>
+                                </div>
+                                <div class="col-12 col-lg-5">
+                                    <div class="form-group">
+                                        <label for="phone_cu">Phone</label>
+                                        <input type="tel" id="phoneEdit" name="phone_cu" class="form-control shadow-sm">
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" id="btnEdit" form="updateCustomer" class="btn btn-warning shadow-sm" onclick="updateCustomer();">Edit</button>
+                        <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Modal's delete -->
+        <div class="modal fade" id="deleteCustomer" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="deleteCustomerLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="deleteCustomerLabel"><strong>Delete Customer</strong></h5>
+                    </div>
+                    <div class="modal-body">
+                        <input type="hidden" id="idCustomerDelete">
+                        <p class="lead">Do you want to delete the customer?</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" id="btnDelete" class="btn btn-warning">Delete</button>
+                        <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script>
+        $(document).ready( function () {
+            confirmDelete();
+
+            $('#dtCustomers').DataTable({
+                responsive: true,
+                ajax:{
+                    url: 'allCustomers',
+                    method: "GET",
+                },
+                columns:[
+                    {data: 'id'},
+                    {data: 'name_cu'},
+                    {data: 'phone_cu'},
+                    {data: 'id',
+                    render: function(data,t,w,meta){
+                        return '<button onclick="editCustomer('+data+');" class="btn btn-xs btn-ligth text-dark mx-1" title="Edit"><i class="fa fa-lg fa-fw fa-pen"></i></button>';
+                    }},
+                    {data: 'id',
+                    render: function(data,t,w,meta){
+                        return '<button class="btn btn-xs btn-ligth text-dark mx-1" title="Delete"><i class="fa fa-lg fa-fw fa-trash" onclick="deleteCustomer('+data+')"></i></button>';
+                    }}
+                ]
+            });
+        } );
+
+        function editCustomer(idCustomer){
+            $.ajax({
+                type: "GET",
+                url: 'viewCustomer/'+idCustomer,
+                success: function(data){
+                    $('#nameEdit').val(data.name_cu);
+                    $('#phoneEdit').val(data.phone_cu);
+                    $('#idCustomerEdit').val(data.id);
+                }
+            })
+            $('#editCustomer').modal('show');
+        }
+
+        function updateCustomer(){
+            $.ajax({
+                headers: {'X-CSRF-TOKEN': $('input[name=_token]').val()},
+                type: "POST",
+                url: $('#idCustomerEdit').val(),
+                data: $('form#updateCustomer').serialize(),
+                success: function(data){
+                    $('#editCustomer').modal('hide');
+                    $('#dtCustomers').DataTable().ajax.reload();
+                }
+            });
+        }
+
+        function deleteCustomer(idCustomer){
+            $('#idCustomerDelete').val(idCustomer);
+            $('#deleteCustomer').modal('show');
+        }
+
+        function confirmDelete(){
+            $('#btnDelete').on('click', function(e){
+                e.preventDefault();
+                fetch('deleteCustomer/'+$('#idCustomerDelete').val(),{
+                    method: "GET"
+                }).then(function(d){
+                    $('#deleteCustomer').modal('hide');
+                    $('#dtCustomers').DataTable().ajax.reload();
+                });
+            });
+        }
+    </script>
+@stop
