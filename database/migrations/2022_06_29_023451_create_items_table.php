7f6ad92856290ebd26241dbdfedc09d801e65a50
Orliany Quezada <orliany.quezada@gmail.com>
2022-07-09 10:20:21 -0400

    modificacion del modulo items

--- database/migrations/2022_06_29_023451_create_items_table.php
+++ database/migrations/2022_06_29_023451_create_items_table.php
@@ -15,19 +15,19 @@ class CreateItemsTable extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
-            $table->datetime('datetime_it');
+            $table->datetime('datetime_input_it');
             $table->text('item_it')->nullable();
             $table->string('quanty_it')->nullable();
             $table->string('qty_boxes_it')->nullable();
             $table->text('ubication_it');
-            $table->text('observation_it')->nullable();
+            $table->text('observation_input_it')->nullable();
+            $table->datetime('datetime_exit_it')->nullable();
+            $table->text('address_it')->nullable();
+            $table->text('observation_exit_it')->nullable();
             $table->foreignId('customer_id')->nullable()->constrained('customers')->onDelete('cascade')->onUpdate('cascade');
             $table->foreignId('sub_customer_id')->nullable()->constrained('customers')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreignId('condition_id')->constrained('conditions')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreignId('status_id')->constrained('status')->onDelete('cascade')->onUpdate('cascade');
             $table->foreignId('shipment_id')->nullable()->constrained('shipments')->onDelete('cascade')->onUpdate('cascade');
             $table->foreignId('employee_id')->nullable()->constrained('users')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreignId('item_id')->nullable()->constrained('items')->onDelete('cascade')->onUpdate('cascade');
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade')->onUpdate('cascade');
             $table->softDeletes();
             $table->timestamps();
